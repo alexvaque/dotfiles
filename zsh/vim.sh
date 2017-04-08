14dbef9dd5f681c1be04767585e6d885faa58c31
alex <alex.vaque@letgo.com>
2017-04-08 18:21:19 +0200

    Adding Plugin

--- zsh/vim.sh
+++ zsh/vim.sh
@@ -1,3 +1,4 @@
 sudo apt-get install vim-puppet
 ~/.vim/plugin/
-ln -s /usr/share/vim/addons/syntax/puppet.vim ~/.vim/plugin/
+#ln -s /usr/share/vim/addons/syntax/puppet.vim ~/.vim/plugin/
+sudo vim-addons -w install puppet
